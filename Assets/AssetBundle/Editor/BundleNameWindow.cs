7a12b1fb5dd081b961629c9b8f1acdb1e0b2fee0
zouhangte <zouhangtezbm@126.com>
2016-11-19 12:01:39 +0800

    添加Bundlename配制脚本

---
+++ Assets/AssetBundle/Editor/BundleNameWindow.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BundleNameWindow : EditorWindow
+{
+    List<AssetImporter> importerList = new List<AssetImporter>();
+    Dictionary<AssetImporter, Object> objectList = new Dictionary<AssetImporter, Object>();
+    Object waitAdd = null;
+    AssetImporter waitDelete;
+    Vector2 scrollPos;
+
+    void LoadAllAssetBundle()
+    {
+        string[] assets;
+        string[] allAssetBundle = AssetDatabase.GetAllAssetBundleNames();
+        for (int i = 0; i < allAssetBundle.Length; i++)
+        {
+            assets = AssetDatabase.GetAssetPathsFromAssetBundle(allAssetBundle[i]);
+            for (int j = 0; j < assets.Length; j++)
+            {
+                AssetImporter importer = AssetImporter.GetAtPath(assets[j]);
+                Debug.Log(assets[j]);
+                Object obj = AssetDatabase.LoadAssetAtPath<Object>(assets[j]);
+                importer.name = obj.name;
+                objectList.Add(importer, obj);
+                importerList.Add(importer);
+            }
+        }
+    }
+
+    void LoadFolderAssetBundle(string path)
+    {
+        List<string> bundleObjs = new List<string>();
+        FileUtility.Recursive(path, action: (x) => { bundleObjs.Add(x); });
+        for (int i = 0; i < bundleObjs.Count; i++)
+        {
+            AssetImporter importer = AssetImporter.GetAtPath(FileUtil.GetProjectRelativePath(bundleObjs[i]));
+            if (importer != null)
+            {
+                if (!string.IsNullOrEmpty(importer.assetBundleName) )
+                {
+                    Object obj = AssetDatabase.LoadAssetAtPath<Object>(importer.assetPath);
+                    importer.name = obj.name;
+                    objectList.Add(importer, obj);
+                    importerList.Add(importer);
+                }
+            }
+         
+        }
+    }
+
+    void OnGUI()
+    {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        for (int i = 0; i < importerList.Count; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            ShowObjectItemInfo(importerList[i]);
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        RemoveIfNeed();
+
+        GUILayout.Label("---");
+        EditorGUILayout.BeginHorizontal();
+
+        ShowControllButtons();
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    void ShowObjectItemInfo(AssetImporter impoter)
+    {
+        objectList[impoter] = EditorGUILayout.ObjectField(objectList[impoter], typeof(Object), false);
+
+        impoter.name = EditorGUILayout.TextField(impoter.name);
+
+        impoter.assetBundleName = EditorGUILayout.TextField(impoter.assetBundleName);
+
+        if (GUILayout.Button("-"))
+        {
+            waitDelete = impoter;
+        }
+       
+    }
+
+    void ShowControllButtons()
+    {
+        waitAdd = EditorGUILayout.ObjectField(waitAdd, typeof(Object), false);
+
+        string name = EditorGUILayout.TextField("assetName");
+
+        string assetBundleName = EditorGUILayout.TextField("assetBundleName");
+
+        if (GUILayout.Button("+") && waitAdd != null)
+        {
+            string path = AssetDatabase.GetAssetPath(waitAdd);
+            AssetImporter impoter = AssetImporter.GetAtPath(path);
+            impoter.assetBundleName = assetBundleName;
+            impoter.name = name;
+            if (!importerList.Find((x)=> x.name == name && x.assetBundleName == assetBundleName))
+            {
+                importerList.Add(impoter);
+                objectList.Add(impoter, waitAdd);
+            }
+        }
+        if (GUILayout.Button("ReloadFolder"))
+        {
+            string folder = EditorUtility.OpenFolderPanel("选择文件夹", Application.dataPath, "");
+            if (folder != null)
+            {
+                importerList.Clear();
+                objectList.Clear();
+                LoadFolderAssetBundle(folder);
+            }
+        }
+        if (GUILayout.Button("ReloadAll"))
+        {
+            importerList.Clear();
+            objectList.Clear();
+            LoadAllAssetBundle();
+        }
+    }
+
+    void RemoveIfNeed()
+    {
+        if(waitDelete != null)
+        {
+            importerList.Remove(waitDelete);
+            objectList.Remove(waitDelete);
+            waitDelete.assetBundleName = "";
+            waitDelete.name = "";
+            waitDelete = null;
+        }
+    }
+}
