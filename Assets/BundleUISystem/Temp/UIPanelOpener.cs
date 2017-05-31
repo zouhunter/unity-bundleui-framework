41efb0f9a1089e3602b3f81df313a39dd34a7a15
zouhangte <zouhangtezbm@126.com>
2017-05-31 15:17:39 +0800

    add UIPanelOpener.cs

---
+++ Assets/BundleUISystem/Temp/UIPanelOpener.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine;
+using BundleUISystem;
+#if UNITY_EDITOR
+using UnityEditor;
+[CustomEditor(typeof(UIPanelOpener)),CanEditMultipleObjects]
+public class UIPanelOpenerDrawer:Editor
+{
+    GUISkin skin;
+    private void OnEnable()
+    {
+        skin = EditorGUIUtility.GetBuiltinSkin(EditorSkin.Inspector);
+    }
+    public override void OnInspectorGUI()
+    {
+        var opener = (UIPanelOpener)target;
+        opener.panelTemp = EditorGUILayout.ObjectField(opener.panelTemp, typeof(UIPanelTemp), true) as UIPanelTemp;
+        if(opener.panelTemp) opener.panelName = opener.panelTemp.name;
+        EditorGUI.BeginDisabledGroup(true);
+        opener.panelName = EditorGUILayout.TextField(opener.panelName);
+        EditorGUI.EndDisabledGroup();
+        opener.destroyOnOpen = EditorGUILayout.Toggle("destroyOnOpen", opener.destroyOnOpen, EditorStyles.toggle);
+    }
+}
+#endif
+public class UIPanelOpener : MonoBehaviour {
+#if UNITY_EDITOR
+public UIPanelTemp panelTemp;
+#endif
+    public string panelName;
+    public bool destroyOnOpen;
+    public void OpenSend(object data)
+    {
+        UIGroup.Open(panelName,data);
+        if(destroyOnOpen)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void Open()
+    {
+        UIGroup.Open(panelName);
+        if (destroyOnOpen)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
