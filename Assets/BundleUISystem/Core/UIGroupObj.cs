475f96491a4a7d98264adef201d4286137faef0c
zouhangte <zouhangtezbm@126.com>
2017-07-04 13:13:31 +0800

    增加UIGroupObj

--- Assets/BundleUISystem/Core/UIGroupObj.cs
+++ Assets/BundleUISystem/Core/UIGroupObj.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using BundleUISystem;
 [CreateAssetMenu(menuName ="生成/UI组")]
 public class UIGroupObj : ScriptableObject {
+	public bool defult;
     public List<UIBundleInfo> bundles = new List<UIBundleInfo>();
     public List<UIGroupObj> groupObjs = new List<UIGroupObj>();
 }
