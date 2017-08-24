402699d2d168018b9e555a510e57521c53add2bb
zouhangte <zouhangtezbm@126.com>
2017-08-24 09:02:27 +0800

    修正路径重复问题

---
+++ Assets/Demos/Scenes/Loading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Loading : MonoBehaviour {
+    public string targetSceneName = "Demo";
+    public string targetSceneBundle = "scene/demo";
+    public Slider progressBar;
+    private void Awake()
+    {
+        AssetBundleLoader.Instence.LoadLevelFromUrlAsync(targetSceneBundle, targetSceneName, false, UpdateProgress);
+    }
+    private void UpdateProgress(float progress)
+    {
+        progressBar.value = progress;
+    }
+}
